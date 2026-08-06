d6b81aef88373715e97c1833b7b4d522a32c43fe
bcaparth89a-dev <bcaparth89@gmail.com>
2026-08-06 20:31:57 +0530

    Update SMTP mailer configuration settings and add server verify callback

--- server/src/config/mailer.js
+++ server/src/config/mailer.js
@@ -2,9 +2,23 @@ import nodemailer from "nodemailer";
 import { env } from "./env.js";
 
 export const mailer = nodemailer.createTransport({
-  service: env.EMAIL_SERVICE,
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+
   auth: {
     user: env.EMAIL_USER,
     pass: env.EMAIL_PASS,
   },
+
+  family: 4,
+});
+
+mailer.verify((error, _success) => {
+  if (error) {
+    console.error("SMTP Connection Error:", error);
+  } else {
+    console.info("SMTP Server is Ready");
+  }
 });
